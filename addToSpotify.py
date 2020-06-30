cb9c989048949bf6ed7b8dc959b08a184e4dba1c
j1moreno <jmoreno.0312@gmail.com>
2020-06-30 16:53:51 -0700

    update code style; add main function

--- addToSpotify.py
+++ addToSpotify.py
@@ -5,7 +5,7 @@ import re
 import argparse
 
 
-def getSpotifyIdsFromFile(file_name):
+def get_spotify_ids_from_file(file_name):
     with open(file_name, encoding="utf-8") as file:
         urls = []
         for line in file:
@@ -24,7 +24,7 @@ def show_tracks(tracks):
               (i, track["artists"][0]["name"], track["name"], track["uri"]))
 
 
-def listAllTracksInPlaylist(sp, playlist):
+def list_all_tracks_in_playlist(sp, playlist):
     print("total tracks", playlist["tracks"]["total"])
     results = sp.playlist(playlist["id"], fields="tracks,next")
     tracks = results["tracks"]
@@ -34,72 +34,80 @@ def listAllTracksInPlaylist(sp, playlist):
         show_tracks(tracks)
 
 
-def getLastTrackId(sp, playlist):
+def get_last_track_id(sp, playlist):
     items = sp.playlist_tracks(playlist["id"])["items"]
     return items[len(items) - 1]["track"]["id"]
 
 
-parser = argparse.ArgumentParser(
-    description="Parse links from text and add them to a playlist")
-parser.add_argument('--input', required=True)
-parser.add_argument('--playlistName', required=True)
-parser.add_argument('--append', action='store_true')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(
+        description="Parse links from text and add them to a playlist")
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--playlistName', required=True)
+    parser.add_argument('--append', action='store_true')
+    args = parser.parse_args()
 
-scope = "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public"
-track_ids_from_file = getSpotifyIdsFromFile(args.input)
-print("parsed " + str(len(track_ids_from_file)) + " track IDs")
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-playlists = sp.current_user_playlists()
-for playlist in playlists["items"]:
-    if playlist["name"] == args.playlistName:
-        print("Found playlist: " + playlist["name"])
-        if playlist["tracks"]["total"] == len(
-                track_ids_from_file) and not args.append:
-            print("playlist is up to date!")
-        else:
-            # check for empty playlist
-            if playlist["tracks"]["total"] == 0 or args.append:
-                print("add all tracks from text")
-                index_to_start_from = 0
+    scope = ("playlist-read-private playlist-read-collaborative "
+             "playlist-modify-private playlist-modify-public")
+    track_ids_from_file = get_spotify_ids_from_file(args.input)
+    print("parsed " + str(len(track_ids_from_file)) + " track IDs")
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    playlists = sp.current_user_playlists()
+    for playlist in playlists["items"]:
+        if playlist["name"] == args.playlistName:
+            print("Found playlist: " + playlist["name"])
+            if playlist["tracks"]["total"] == len(
+                    track_ids_from_file) and not args.append:
+                print("playlist is up to date!")
             else:
-                # if playlist is not empty, get last id currently in the playlist
-                last_track_id = getLastTrackId(sp, playlist)
-                print("last track id in playlist is", last_track_id)
-                # find this id in the list of id pulled from file, and get its last index (last because it may be here more than once)
-                all_matches = [
-                    i for i, e in enumerate(track_ids_from_file)
-                    if e == last_track_id
-                ]
-                if len(all_matches) < 1:
-                    # last track in text not found in plist, this is a different set of tracks
-                    # should be appended instead
-                    raise Exception(
-                        "parsed links don't match playlist contents. use --append option to add new set of tracks"
-                    )
-                last_index = all_matches[len(all_matches) - 1]
-                # continue adding tracks to the playlist based on this index
-                index_to_start_from = last_index + 1
-                if index_to_start_from == len(track_ids_from_file):
-                    # there are no new tracks to add, don't bother trying
-                    print("no new tracks to add!")
-                    exit()
-            # check for track limit 100; if exceeded, split into multiple calls
-            sp.trace = False
-            user_id = sp.me()['id']
-            tracks_to_add = track_ids_from_file[index_to_start_from:]
-            if len(tracks_to_add) >= 100:
-                chunks = [
-                    tracks_to_add[x:x + 100]
-                    for x in range(0, len(tracks_to_add), 100)
-                ]
-                for chunk in chunks:
+                # check for empty playlist
+                if playlist["tracks"]["total"] == 0 or args.append:
+                    print("add all tracks from text")
+                    index_to_start_from = 0
+                else:
+                    # if playlist is not empty, get last id currently in the
+                    # playlist
+                    last_track_id = get_last_track_id(sp, playlist)
+                    print("last track id in playlist is", last_track_id)
+                    # find this id in the list of id pulled from file, and get
+                    # its last index (last because it may be here more than once)
+                    all_matches = [
+                        i for i, e in enumerate(track_ids_from_file)
+                        if e == last_track_id
+                    ]
+                    if len(all_matches) < 1:
+                        # last track in text not found in plist, this is a
+                        # different set of tracks; should be appended instead
+                        raise Exception(
+                            ("parsed links don't match playlist contents. "
+                             "use --append option to add new set of tracks"))
+                    last_index = all_matches[len(all_matches) - 1]
+                    # continue adding tracks to the playlist based on this index
+                    index_to_start_from = last_index + 1
+                    if index_to_start_from == len(track_ids_from_file):
+                        # there are no new tracks to add, don't bother trying
+                        print("no new tracks to add!")
+                        exit()
+                # check for track limit 100; if exceeded,
+                # split into multiple calls
+                sp.trace = False
+                user_id = sp.me()['id']
+                tracks_to_add = track_ids_from_file[index_to_start_from:]
+                if len(tracks_to_add) >= 100:
+                    chunks = [
+                        tracks_to_add[x:x + 100]
+                        for x in range(0, len(tracks_to_add), 100)
+                    ]
+                    for chunk in chunks:
+                        results = sp.user_playlist_add_tracks(
+                            user_id, playlist["id"], chunk)
+                        print(results)
+                else:
                     results = sp.user_playlist_add_tracks(
-                        user_id, playlist["id"], chunk)
+                        user_id, playlist["id"], tracks_to_add)
                     print(results)
-            else:
-                results = sp.user_playlist_add_tracks(user_id, playlist["id"],
-                                                      tracks_to_add)
-                print(results)
+            break
 
-        break
+
+if __name__ == "__main__":
+    main()
